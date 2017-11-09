1bc1163e0615af58bd8a70a02490ab8a48f27b9d
hunter10 <hunter10@naver.com>
2017-11-09 11:14:10 +0900

    유니티 5.4.3 프로젝트 올림(버전 맞춰서 작업)

---
+++ Assets/Scripts/CenterCube.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CenterCube : MonoBehaviour {
+
+    // 게임스타트 버튼 이벤트 받으면
+    // 48장 카드 랜덤하게 섞어서 가운데에 쌓기
+    // 오브젝트 풀 사용 준비
+
+    public GameObject cardBase;
+
+    float cardheight = 0.02f;
+
+    List<GameObject> _cardDesk = new List<GameObject>();
+    public List<Transform> _userPos = new List<Transform>();
+
+
+    public void MakeDeck()
+    {
+        for (int i = 0; i < 48; i++)
+        {
+            GameObject card = Instantiate(cardBase, new Vector3(transform.position.x, transform.position.y + cardheight * i, transform.position.z), Quaternion.identity) as GameObject;
+            card.transform.parent = this.transform;
+            _cardDesk.Add(card);
+        }
+    }
+
+    public void MoveToUser()
+    {
+        _cardDesk[0].GetComponent<CardBase>().StartMove(_userPos[0]);
+        _cardDesk[1].GetComponent<CardBase>().StartMove(_userPos[1]);
+    }
+}
+
